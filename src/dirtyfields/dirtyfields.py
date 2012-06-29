f70368cc2f417c82b55d0d84fbcf2f63ed6bae08
George Marshall <george@socialups.com>
2012-06-29 13:25:20 -0700

    Fixed dict generators removing list generator

--- src/dirtyfields/dirtyfields.py
+++ src/dirtyfields/dirtyfields.py
@@ -10,11 +10,11 @@ class DirtyFieldsMixin(object):
         reset_state(sender=self.__class__, instance=self)
 
     def _as_dict(self):
-        return dict([(f.name, f.to_python(getattr(self, f.attname))) for f in self._meta.local_fields])
+        return dict((f.name, f.to_python(getattr(self, f.attname))) for f in self._meta.local_fields)
 
     def get_dirty_fields(self):
         new_state = self._as_dict()
-        return dict([(key, value) for key, value in self._original_state.iteritems() if value != new_state[key]])
+        return dict((k, v) for k, v in self._original_state.iteritems() if v != new_state[k])
 
     def is_dirty(self):
         # in order to be dirty we need to have been saved at least once, so we
