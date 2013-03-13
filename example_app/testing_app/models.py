a8b934d929418999e71743c596f2169e84105b71
Corey Oordt <coreyoordt@gmail.com>
2013-03-13 13:37:43 -0500

    Updated tests and made sure they work

--- example_app/testing_app/models.py
+++ example_app/testing_app/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 from dirtyfields import DirtyFieldsMixin
 
-class TestModel(DirtyFieldsMixin, models.Model):
-    """A simple test model to test dirty fields mixin with"""
+
+class ForeignTestModel(DirtyFieldsMixin, models.Model):
+    """
+    A simple test model with which to test foreign keys and dirty fields mixin
+    """
     boolean = models.BooleanField(default=True)
     characters = models.CharField(blank=True, max_length=80)
-    
+
+
+class TestModel(DirtyFieldsMixin, models.Model):
+    """
+    A simple test model with which to test dirty fields mixin
+    """
+    boolean = models.BooleanField(default=True)
+    characters = models.CharField(blank=True, max_length=80)
+    foreign_test_model = models.ForeignKey(ForeignTestModel, blank=True, null=True)
--- example_app/testing_app/tests.py
+++ example_app/testing_app/tests.py
@@ -1,42 +1,45 @@
-from django.core.management.color import no_style
-from django.db import models
 from django.test import TestCase
-from django.conf import settings
-from django.core.management import call_command
-from django.db.models.loading import load_app
-from django.db import connection, transaction
 
-from example_app.testing_app.models import TestModel
+from example_app.testing_app.models import TestModel, ForeignTestModel
+
 
 class DirtyFieldsMixinTestCase(TestCase):
-    
+
     def test_dirty_fields(self):
         tm = TestModel()
         # initial state shouldn't be dirty
-        self.assertEqual(tm.get_dirty_fields(), {})
-        
+        self.assertEqual(tm.dirty_fields, tuple())
+
         # changing values should flag them as dirty
         tm.boolean = False
         tm.characters = 'testing'
-        self.assertEqual(tm.get_dirty_fields(), {
-            'boolean': True,
-            'characters': ''
-        })
-        
+        self.assertEqual(set(tm.dirty_fields), set(('boolean', 'characters')))
+
         # resetting them to original values should unflag
         tm.boolean = True
-        self.assertEqual(tm.get_dirty_fields(), {
-            'characters': ''
-        })
-    
+        self.assertEqual(tm.dirty_fields, ('characters', ))
+
     def test_sweeping(self):
         tm = TestModel()
         tm.boolean = False
         tm.characters = 'testing'
-        self.assertEqual(tm.get_dirty_fields(), {
-            'boolean': True,
-            'characters': ''
-        })
+        self.assertEqual(set(tm.dirty_fields), set(('boolean', 'characters')))
         tm.save()
-        self.assertEqual(tm.get_dirty_fields(), {})
-    
+        self.assertEqual(tm.dirty_fields, tuple())
+
+    def test_foreignkeys(self):
+        ftm1 = ForeignTestModel(characters="foreign1")
+        ftm1.save()
+        ftm2 = ForeignTestModel(characters="foreign2")
+        ftm2.save()
+        tm = TestModel()
+        tm.boolean = False
+        tm.characters = 'testing'
+        tm.foreign_test_model = ftm1
+        self.assertEqual(set(tm.dirty_fields), set(('boolean', 'characters', 'foreign_test_model')))
+        tm.save()
+        self.assertEqual(tm.dirty_fields, tuple())
+        tm.foreign_test_model = ftm2
+        self.assertEqual(tm.dirty_fields, ('foreign_test_model', ))
+        tm.foreign_test_model.characters = "foreign2.0"
+        self.assertEqual(tm.foreign_test_model.dirty_fields, ('characters', ))
